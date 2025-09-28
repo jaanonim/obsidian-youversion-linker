9922e441b08c5376ea13af32d1013c4541df2f74
jaanonim <mat8mro@gmail.com>
2025-09-28 20:20:22 +0200

    fix for escaping >

--- src/verses/VerseEmbed.ts
+++ src/verses/VerseEmbed.ts
@@ -11,7 +11,8 @@ export default class VerseEmbed extends Verse {
 		chapter: number,
 		verses: Array<VerseElement>,
 		private insertNewLine: boolean,
-		private calloutName: string
+		private calloutName: string,
+		private type: any
 	) {
 		super(version, bookUrl, book, chapter, verses);
 	}
@@ -23,7 +24,7 @@ export default class VerseEmbed extends Verse {
 			return `${p}>[!Error] Cannot get content of ${this.toSimpleText()}.\n`;
 		} else {
 			// prettier-ignore
-			return `${p}>[!${this.calloutName}] [${this.toSimpleText()} ${content.info.version}](${this.getUrl()})\n>${escapeMarkdown(content.verses.replace(/\n/g,'\n>'))}\n`;
+			return `${p}>[!${this.calloutName}] [${this.toSimpleText()} ${content.info.version}](${this.getUrl()})\n>${escapeMarkdown(content.verses).replace(/\n/g,'\n>')}\n`;
 		}
 	}
 }
