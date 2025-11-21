eff0a1739c6676adf7489cd421bc7e96e9d4f653
jaanonim <mat8mro@gmail.com>
2025-11-21 13:51:27 +0100

    better version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,6 +1,7 @@
+git pull
 pnpm run version
 git add .
 npm_package_version=$(node -p "require('./package.json').version")
 git commit -m "v${npm_package_version}"
-git tag "v${npm_package_version}"
-git push origin main --tags
+git tag "v${npm_package_version}" -m "v${npm_package_version}"
+git push --follow-tags
