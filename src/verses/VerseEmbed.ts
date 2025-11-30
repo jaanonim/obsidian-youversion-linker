31ad2b4de579f68462d0369f424fe909486da5e6
reillyfox <9413043+foxcub99@users.noreply.github.com>
2025-11-30 11:57:14 +1100

    Remove redundant comments

--- src/verses/VerseEmbed.ts
+++ src/verses/VerseEmbed.ts
@@ -26,8 +26,24 @@ export default class VerseEmbed extends Verse {
 		if (content.err) {
 			return `${p}>[!Error] Cannot get content of ${this.toSimpleText()}.\n`;
 		} else {
+			let calloutIcon = this.showBibleIcon 
+				? `[!${this.calloutName}]`
+				: this.calloutName;
+			
+			if (this.showBibleIcon && this.collapsibleVerses) {
+				if (this.collapsedByDefault) {
+					calloutIcon += '-';
+				} else {
+					calloutIcon += '+';
+				}
+			}
+
+			const versionText = this.showTranslation 
+				? ` ${content.info.version}` 
+				: '';
+			
 			// prettier-ignore
-			return `${p}>[!${this.calloutName}] [${this.toSimpleText()} ${content.info.version}](${this.getUrl()})\n>${escapeMarkdown(content.verses).replace(/\n/g,'\n>')}\n`;
+			return `${p}>${calloutIcon} [${this.toSimpleText()}${versionText}](${this.getUrl()})\n>${escapeMarkdown(content.verses).replace(/\n/g,'\n>')}\n`;
 		}
 	}
 }
