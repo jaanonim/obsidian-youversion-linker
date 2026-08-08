50f0c26bc9dd7193f11361eaf6c691fcb6816761
jaanonim <mat8mro@gmail.com>
2026-08-08 14:10:42 +0200

    Better version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,8 +1,32 @@
-git pull
+#!/bin/sh
+
+set -e
+
+if [ "$(git branch --show-current)" != "master" ]; then
+    echo "Need to be on master branch to run this script."
+    exit 1
+fi
+
 npm run version
-git add .
+npm run check
 npm_package_version=$(node -p "require('./package.json').version")
+
+echo "Are you sure you want to commit ${npm_package_version}? (y/N)"
+read answer
+if [[ $answer != "y" ]]; then
+  echo "Aborting."
+  exit 1
+fi
+git add .
 git commit -m "v${npm_package_version}"
 git tag "v${npm_package_version}" -m "v${npm_package_version}"
+
+echo "Are you sure you want to push ${npm_package_version}? (y/N)"
+read answer
+if [[ $answer != "y" ]]; then
+  echo "Aborting."
+  exit 1
+fi
 git push
 git push origin --tags
+echo "DONE!"
