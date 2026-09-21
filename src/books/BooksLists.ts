636971a0ea06cb0b7290d1a60fbe4d5f82a8c12e
Sebastian <sebastian.w.andrzejczak@gmail.com>
2026-09-21 08:31:35 +0300

    Update BooksLists.ts

--- src/books/BooksLists.ts
+++ src/books/BooksLists.ts
@@ -1,5 +1,5 @@
 import am from '../../data/books/am.json';
-import bg from '../../data/books/bg.json'
+import bg from '../../data/books/bg.json';
 import cz from '../../data/books/cz.json';
 import da from '../../data/books/da.json';
 import en from '../../data/books/en.json';
